1da611b417885ba162d751afcd04568375ad6bb4
Matthew Singleton <mattmega4@gmail.com>
2017-02-23 15:32:51 -0800

    first commit m

---
+++ WhenThen/LandingPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LandingPageViewController.swift
+//  WhenThen
+//
+//  Created by Matthew Singleton on 2/23/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
